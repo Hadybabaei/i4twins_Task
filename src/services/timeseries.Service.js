97c4d83339c7d2f5996935599478f65eb8b887aa
Hady Babaei <mr.hady.ba69@gmail.com>
2023-06-27 15:37:42 +0330

    uploading latest

--- src/services/timeseries.Service.js
+++ src/services/timeseries.Service.js
@@ -14,7 +14,6 @@ exports.getAllTimeseries = async (startTime, endTime) => {
 
   const projection = { _id: 0, timestamp: 1, data: 1 };
   
-  // Utilize indexes for faster querying
   const timeseriesData = await Timeseries.find(query, projection)
     .sort({ timestamp: 1 })
     .lean();
@@ -25,7 +24,7 @@ exports.getAllTimeseries = async (startTime, endTime) => {
 //   const query = { timestamp: { $gte: startTime, $lte: endTime } };
 //   const projection = { _id: 0, timestamp: 1, data: 1 };
 
-//   // Utilize indexes for faster querying
+
 //   const timeseriesData = await Timeseries.find(query, projection)
 //     .sort({ timestamp: 1 })
 //     .lean();
